c8c7983e761ac17f79e28eed872b876f248d8840
kinantatradersfamily <kinanta.tradersfamily@gmail.com>
2023-11-02 10:48:26 +0700

    add navbar

--- resources/views/client/layout/navbar.blade.php
+++ resources/views/client/layout/navbar.blade.php
@@ -5,13 +5,47 @@
       align-items: center;"> <img src="/assets/client/img/Screenshot_2023-06-21_at_23.55.06-removebg-preview.png" alt="" style="height: auto; width: 100%; max-width: 5vw; "> <span style="font-family: 'Montserrat'; font-weight: 1000; font-size: 2.5vw;font-weight: 500;margin-left:10px;">PT CATUR KREASI AKSARA </span> </a>
       <nav id="navbar" class="navbarResponsive" >
           <ul>
-            <li><a class="nav-link" href="{{url('/')}}">Beranda</a></li>
-            <li><a class="nav-link" href="{{url('/product')}}">Layanan Kami</a></li>
-            <li><a class="nav-link" href="{{url('/equipment')}}">Equipment</a></li>
-            <li><a class="nav-link" href="{{url('/contact')}}">Hubungi Kami</a></li>
-            <li><a class="nav-link" href="{{url('/about')}}">Tentang Kami</a></li>
+            <li>
+              <a class="nav-link" href="{{url('/')}}">Home</a>
+              <div class="text-left" id="mantabek" style="font-weight: 600;padding-left: 5px; padding-top: 8px; color: #6C5F5B; font-size: 15px">
+                <span>Beranda</span>
+              </div>
+            </li>
+            <li>
+              <a class="nav-link" href="{{url('/product')}}">Product</a>
+              <div class="text-left" id="mantabek" style="font-weight: 600;padding-left: 5px; padding-top: 8px; color: #6C5F5B; font-size: 15px">
+                <span>Produk</span>
+              </div>
+             </li>
+            <li><a class="nav-link" href="{{url('/equipment')}}">Equipment</a>
+              <div class="text-left" id="mantabek" style="font-weight: 600;padding-left: 5px; padding-top: 8px; color: #6C5F5B; font-size: 15px">
+                <span>Perlengkapan</span>
+              </div>
+            </li>
+            <li><a class="nav-link" href="{{url('/contact')}}">Contact Us</a>
+              <div class="text-left" id="mantabek" style="font-weight: 600;padding-left: 5px; padding-top: 8px; color: #6C5F5B; font-size: 15px">
+                <span>Hubungi Kami</span>
+              </div>
+            </li>
+            <li><a class="nav-link" href="{{url('/about')}}">Contact Us</a>
+              <div class="text-left" id="mantabek" style="font-weight: 600;padding-left: 5px; padding-top: 8px; color: #6C5F5B; font-size: 15px">
+                <span>Tentang Kami</span>
+              </div>
+            </li>
           </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
     </div>
   </header>
+
+  <style>
+    .nav-link span {
+      margin-top: 10px
+    }
+
+    @media screen and (max-width: 990px) {
+      #mantabek span {
+        margin-left: 15px;
+      }
+    }
+  </style>
